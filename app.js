957de211cfbf46ea576b092ca3e30db5e7e40b4c
Zarina171717 Shixaliyeva <sixeliyeva05@gmail.com>
2024-03-26 20:56:18 +0000

    minibox

--- app.js
+++ app.js
@@ -2,7 +2,13 @@
 $(document).ready(function () {
     $('.btn').click(function (e) { 
         e.preventDefault();
-
-        
+        if (!$.trim($('input').val()) == '') {
+            let a = b => {
+                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`); }
+            }
+            let v = f => Number(f) ? a(f) : $('.minibox').append(`<h2>${f}</h2>`);
+            v($('input').val());
+        }
+        $('input').val('');
     });    
 });
